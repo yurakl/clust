9b18fc4fb2a6a32cf85cd484ab82f1184434f7f1
yurakl <yuraklisko@gmail.com>
2023-09-07 13:46:31 +0300

    clust-queue: add scheduling into file

--- common.h
+++ common.h
@@ -1,15 +1,39 @@
-#define LEN[100]
-#define FNLEN[100]
-#define PATHLEN[300]
-typedef struct ClustQueue {
-	pid_t pid;
+#include <string.h>
+#include <fcntl.h>
+#include <sys/file.h>
+
+#define LEN 	100
+#define FNLEN 	100
+#define PATHLEN 300 
+
+#define QFORMAT(T) "%u %d %s %s %s %s\n", T.pid, T.nproc, T.inputFile, T.logFile, T.owner, T.workingFolder
+
+typedef struct{
+	pid_t	pid;
 	char	owner[LEN];
 	char	workingFolder[PATHLEN];
 	char	inputFile[FNLEN];
 	char	logFile[FNLEN];
-	int	nproc;
-};
+	int		nproc;
+} ClustQueue;
 
-char HomeFolderName[]	= "~/.clust";
-char HostFileName[]	= "/etc/hosts";
+char HomeFolderName[]	= "/home/mpiuser/.clust";
+char QueueFile[PATHLEN];
 
+char HostFileName[]		= "/etc/hosts";
+
+int lockFile(int fd) {
+    if (flock(fd, LOCK_EX | LOCK_NB) == -1) {
+        perror("Error while locking Queue file");
+        return 0;
+    }
+    return 1;
+} 
+
+int unlockFile(int fd) {
+    if (flock(fd, LOCK_UN) == -1) {
+        perror("Error while unlocking Queue file");
+        return 0;
+    }
+    return 1;
+}
